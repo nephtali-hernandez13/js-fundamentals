fe3ed4db5ac1d93d4e6657ac241af61cf61c2ec1
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-12 13:10:51 -0700

    UPD functions and scope

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -9,9 +9,97 @@
 
 startCar(123); */
 
+/*
+// SCOPE
 let message = 'Outside';
 if(5 === 5) {
     let message = 'Equal';
     console.log(message);
 }
-console.log(message);
+console.log(message); */
+
+/*
+// IIFE's
+let app = (function() {
+    let carId = 123;
+    console.log('in function');
+    return {};
+})();
+
+console.log(app);
+*/
+
+/*
+// Closures
+let app = (() => {
+    let carId = 123;
+    let getId = () => { return carId; };
+    return { getId: getId };
+})();
+console.log( app.getId() );
+
+let app = (() => {
+    let nombre = "N. Hernandez";
+    let getNombre = () => { return nombre; };
+    return { getNombre: getNombre };
+})();
+console.log( app.getNombre() );
+*/
+
+/*
+// The this keyword
+let fn = () => {
+    console.log (this === window);
+} 
+fn();
+
+let o = {
+    carId: 123,
+    getId: function() {
+        console.log(this);
+        return this.carId;
+    }
+};
+console.log( o.getId() );
+*/
+/*
+// Call and apply
+let o = {
+    carId: 123,
+    getId: function(prefix) {
+        return prefix + this.carId;
+    }
+};
+
+let newCar = { carId:456 };
+console.log( o.getId.apply(newCar, ['ID: ']) );¨
+*/
+
+// Bind
+/* 
+let o = {
+    carId: 123,
+    getId: function() {
+        return this.carId;
+    }
+};
+
+let newCar = { carId: 760 };
+let newFn = o.getId.bind(newCar);
+
+console.log( newFn() );
+*/
+
+// Arrow functions
+//let getId = (prefix, suffix) => prefix + 123 + suffix;
+//console.log( getId('ID: ', '!') );
+//let getId = _ => 123; console.log( getId() );
+
+// Default parameters
+
+let trackCar = function(carId, city='NY') {
+    console.log(`Tracking ${carId} in ${city}.`);
+}
+
+console.log( trackCar(123) );
+console.log( trackCar(123, 'Chicago') );
