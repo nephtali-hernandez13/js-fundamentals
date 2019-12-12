263579bc2679ffb4f69fe13dd1b50ba3c3a1e18f
Nephtali Abisai Hernández Olvera <sistemas.naho13@gmail.com>
2019-12-12 14:26:45 -0700

    Add programming bom and dom

---
+++ programming-bom-and-dom.js
@@ -0,0 +1,27 @@
+// Timers
+
+/* let timeoutId = setTimeout (() => {
+    console.log('1 Second passed');
+}, 1000);
+
+let intervalId = setInterval(() => {
+    console.log('1 Second passed');
+}, 1000); */
+
+// if need to cancel
+//clearInterval(intervalId);
+
+// The location object
+//console.log(document.location.href);
+
+//let el = document.getElementById('first');
+//let el = document.getElementsByClassName('p1');
+//let el = document.getElementsByTagName('p');
+//console.log(el[0]);
+
+let element = document.getElementById('first');
+//element.textContent = 'New content...';
+//element.setAttribute('foo', 'fooValue');
+//element.classList.add('p2');
+element.style.color = 'blue';
+console.log(element);
